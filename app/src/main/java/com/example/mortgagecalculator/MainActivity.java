deae6518e72ea5657705ab6b8e2086102b3f7d3b
nanasei69 <samuelanane62624@gmail.com>
2019-04-01 02:37:01 +0000

    done

---
+++ app/src/main/java/com/example/mortgagecalculator/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.mortgagecalculator;
+
+import android.content.Intent;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static Mortgage mortgage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mortgage = new Mortgage( this );
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onStart ( ) {
+        super.onStart( );
+        updateView( );
+    }
+
+    public void updateView( ) {
+        TextView amountTV = ( TextView ) findViewById(  R.id.amount );
+
+        amountTV.setText(  mortgage.getFormattedAmount( ) );
+
+        TextView yearsTV = ( TextView ) findViewById(  R.id.years );
+
+        yearsTV.setText( "" + mortgage.getYears( ) );
+
+        TextView rateTV = ( TextView ) findViewById( R.id.rate );
+
+        rateTV.setText( 100 * mortgage.getRate( ) + "%" );
+
+        TextView monthlyTV = ( TextView ) findViewById( R.id.payment );
+
+        monthlyTV.setText( mortgage.formattedMonthlyPayment( ) );
+
+        TextView totalTV = ( TextView ) findViewById( R.id.total );
+
+        totalTV.setText( mortgage.formattedTotalPayment( ) );
+    }
+
+    public void modifyData ( View v ) {
+        Intent myIntent = new Intent( this, DataActivity.class );
+        this.startActivity( myIntent );
+        overridePendingTransition ( R.anim.slide_from_left, 0 );
+    }
+
+
+}
